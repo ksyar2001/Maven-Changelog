fa97be913e21647974cad0242124f10a468ea3dc
Sung H Kang <love.power.yeah@gmail.com>
2018-06-17 17:54:44 -0500

    feat(component1) Initializing the project

---
+++ src/main/java/com/codebind/changelog_demo/App.java
@@ -0,0 +1,13 @@
+package com.codebind.changelog_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
